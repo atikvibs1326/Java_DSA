9514df23fb95479b9ffce5419b29fa0c5a25ec45
ATIKRAJA AMANULLA KHOCHIKAR <64660852+atikvibs1326@users.noreply.github.com>
2023-04-08 13:51:26 +0530

    Create Main.java

---
+++ Strings and StringBuilder/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        String a="Atik";
+        System.out.println(a);
+        a="khochikar";
+        System.out.println(a);
+        System.out.println(Math.pow(625,1/2)+Math.sqrt(144));
+
+    }
+}

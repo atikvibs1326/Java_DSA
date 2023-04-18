ba3e2ec1e8ff0ddee4da33aef73746e9b6cd61ad
ATIKRAJA AMANULLA KHOCHIKAR <64660852+atikvibs1326@users.noreply.github.com>
2023-04-18 20:50:51 +0530

    Create Que1.java

---
+++ Patterns/Que1.java
@@ -0,0 +1,24 @@
+package com.atik;
+//Q
+//    *****
+//    *****
+//    *****
+//    *****
+//    *****
+public class Que1 {
+    public static void main(String[] args) {
+            pattern2(5);
+    }
+    static void pattern2(int n){
+        //Step1- outer loop will run the no of lines present in the Q
+        for (int i = 1; i <=n ; i++) {
+
+            //step-2 identify for every row no,run the col
+            for (int j = 1; j <=n ; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}

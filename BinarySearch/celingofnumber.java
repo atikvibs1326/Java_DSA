db1d58a3ef84e6bd8a74b0b79e6ad86a746b9a38
ATIKRAJA AMANULLA KHOCHIKAR <64660852+atikvibs1326@users.noreply.github.com>
2023-04-26 19:51:59 +0530

    Create celingofnumber.java

---
+++ BinarySearch/celingofnumber.java
@@ -0,0 +1,39 @@
+package com.atik;
+
+import java.util.Arrays;
+
+public class ceiling {
+    public static void main(String[] args) {
+        int[] arr={2,3,5,9,14,16,18};
+        int target=13;
+        System.out.println(Arrays.toString(ceiling(arr,target)));
+    }
+    static int[] ceiling(int[] arr, int target){
+
+        //but what if the target number is the greater than the greatest number in the array
+        // in that case
+        if(target>arr[arr.length-1]){
+            return new int[]{-1};
+        }
+        int start=0;
+        int end=arr.length-1;
+
+        //find the mid
+        while(start<=end){
+
+            //effective way of finding mid
+            int mid= start+(end-start)/2;
+
+            if(target==arr[mid]){
+                return new int[]{mid}; //found the target index of mid also the best case
+            }else if(target<arr[mid]){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }
+        return new int[]{start,arr[start]};
+    }
+}
+
+

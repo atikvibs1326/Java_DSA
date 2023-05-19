8f6e0fe07abf683b7995061a81c0ba99394024b0
ATIKRAJA AMANULLA KHOCHIKAR <64660852+atikvibs1326@users.noreply.github.com>
2023-05-19 21:19:38 +0530

    Factorial of a number

---
+++ functions/Factorial.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+class Factorial{
+  public static void main(String[] args){
+  System.out.println("Enter the no to find the factorial:");
+  Scanner in = new Scanner(System.in);
+  int no=in.nextInt();
+  System.out.println("The factorial of "+no+"is"+factorial(no));
+  }
+  //using forloop
+  static int factorial(int n){
+      int fact=1;
+      for(int i=1;i<=n;i++){
+          fact=fact*i;  //OR   fact*=i;
+      }
+    return fact;
+  }
+  //using whileloop
+  static int factorial2(int n){
+    int fact=1;
+    int i=1;
+    while(i<=n){
+      fact=fact*i;
+      i++;
+    }
+    return fact;
+  }
+  
+}
